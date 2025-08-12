b2930ffa144efd5f31567bfa09ff670eddbdf694
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-08-12 14:57:20 +0300

    First commit

---
+++ Portofolio/frontendvue/src/router/index.js
@@ -0,0 +1,23 @@
+import AboutSection from '@/views/AboutSection.vue'
+import BlogSection from '@/views/BlogSection.vue'
+import ContactSection from '@/views/ContactSection.vue'
+import HeroSection from '@/views/HeroSection.vue'
+import ResumeSection from '@/views/ResumeSection.vue'
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+Vue.use(Router)
+
+export default  new Router({
+    mode: 'history',
+
+    routes: [
+        { path: '/', name: 'HeroSection', component: HeroSection},
+        { path: '/about', name: 'AboutSection', component: AboutSection},
+        { path: '/projects', name: 'ProjectSection', component: ProjectSection },
+        { path: '/resume', name: 'ResumeSetion', component: ResumeSection},
+        { path: '/blog', name: 'BlogSection', component: BlogSection},
+        { path: '/contact', name: 'ContactSection', component: ContactSection},
+    ]
+})
