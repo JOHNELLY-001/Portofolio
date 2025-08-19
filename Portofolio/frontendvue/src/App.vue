0d9a3cf06c157f9e94d395c21d2c9bd87b36d4c4
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-08-19 15:37:37 +0300

    Added rendering to a single page as SPA

--- Portofolio/frontendvue/src/App.vue
+++ Portofolio/frontendvue/src/App.vue
@@ -1,15 +1,56 @@
 <template>
   <div :class="theme">
     <Navbar :theme="theme" @toggle-theme="toggleTheme" />
-    <router-view />
+    <!-- <router-view /> -->
+
+    <!-- Other components -->
+     <section id="home">
+       <HeroSection/>
+     </section>
+
+     <section id="about">
+       <AboutSection/>
+     </section>
+
+     <section id="projects">
+       <ProjectSection/>
+     </section>
+
+     <section id="resume">
+       <ResumeSection/>
+     </section>
+
+     <section  id="blog">
+       <BlogSection/>
+     </section>
+
+     <section id="contact">
+       <ContactSection/>
+     </section>
+
   </div>
 </template>
 
 <script>
 import Navbar from './components/NavbarSection.vue'
+import AboutSection from './views/AboutSection.vue';
+import BlogSection from './views/BlogSection.vue';
+import ContactSection from './views/ContactSection.vue';
+import HeroSection from './views/HeroSection.vue';
+import ProjectSection from './views/ProjectSection.vue';
+import ResumeSection from './views/ResumeSection.vue';
 
 export default {
-  components: { Navbar },
+  components: { 
+    Navbar,
+    HeroSection,
+    AboutSection,
+    ProjectSection,
+    ResumeSection,
+    BlogSection,
+    ContactSection,
+   },
+
   data() {
     return { theme: 'dark' }
   },
@@ -25,9 +66,11 @@ export default {
 .dark {
   background-color: black;
   color: white;
+  transition: background-color 0.3s ease, color 0.3s ease;
 }
 .light {
   background-color: white;
   color: black;
+  transition: background-color 0.3s ease, color 0.3s ease;
 }
 </style>
