02f6fdce84be00df96ac88d25b370b54f9f43b16
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-08-19 15:27:47 +0300

    No longer used

---
+++ Portofolio/frontendvue/src/components/FooterBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- Socials -->
+      <div class="flex space-x-4 text-3xl text-gray-400 mt-6">
+        <a href="#" class="hover:text-blue-700"><i class='bx bxl-facebook'></i></a>
+        <a href="#" class="hover:text-white"><i class='bx bxl-github'></i></a>
+        <a href="#" class="hover:text-blue-600"><i class='bx bxl-linkedin'></i></a>
+        <a href="#" class="hover:text-green-500"><i class='bx bxl-whatsapp'></i></a>
+        <a href="#" class="hover:text-pink-600"><i class='bx bxl-instagram'></i></a>
+        <a href="#" class="hover:text-sky-400"><i class='bx bxl-twitter'></i></a>
+      </div>
+</template>
